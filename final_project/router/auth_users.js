185e64ce3e8749a716ef1ff4e0e3cd746f5dceda
Sachin Adat <sachinadat@gmail.com>
2026-06-01 17:38:57 -0400

    Add Task 8

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -20,8 +20,10 @@ regd_users.post("/login", (req,res) => {
     if (username && password) {
 
         if (authenticatedUser(username, password)) {
-            const accesToken = jwt.sign({data: password}, 'access', {expiresIn: 60 * 60});
-            req.session.authorization = {accesToken, username}
+            const accessToken = jwt.sign({data: password}, 'access', {expiresIn: 60 * 60});
+            req.session.authorization = {
+                accessToken, username
+            };
             return res.status(200).json({message: "User successfully logged in!"});
         } else {
             return res.status(208).json({message: "Invalid Username or Password!"});
@@ -35,9 +37,11 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
     const isbn = req.params.isbn;
-    console.log('=============>' + req.session.authorization.username )
-    //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const review = req.body.review;
+    const book = books[isbn];
+    const username = req.session.authorization.username;
+    book.reviews[username] = review;
+    return res.status(200).json({message: "Review saved successfully"});
 });
 
 module.exports.authenticated = regd_users;

7fe9641734158d6accc2d8d2e506400b98ca31dd
Sachin Adat <sachinadat@gmail.com>
2026-06-02 01:56:08 -0400

    Add Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -44,6 +44,15 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     return res.status(200).json({message: "Review saved successfully"});
 });
 
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    const book = books[isbn];
+    delete book.reviews[username];
+    return res.status(200).json({message: "Review deleted successfully"});
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
